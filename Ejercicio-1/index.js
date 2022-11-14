24237374b7e6862c622199cecbcfe2a8c4e2108f
Gabriel Sotto <gabrielsotto90@gmail.com>
2022-11-14 15:11:53 -0300

    First exercise

---
+++ Ejercicio-1/index.js
@@ -0,0 +1,24 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const list = [
+    "Gabriel",
+    32,
+    true,
+    new Date(1990, 8, 17),
+    {
+        titulo: "Sherlock Holmes",
+        autor: "Sir Arthur Conan Doyle",
+        fecha: new Date(1982, 11, 14),
+        url: "https://www.cuspide.com/9789563162042/Las+Aventuras+De+Sherlock+Holmes"
+    },
+];
