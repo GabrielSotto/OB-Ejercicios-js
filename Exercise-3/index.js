9c3d2cccdb76e0bf97ce001e24f6d054d21069a8
Gabriel Sotto <gabrielsotto90@gmail.com>
2022-11-17 15:47:03 -0300

    Third exercise

---
+++ Exercise-3/index.js
@@ -0,0 +1,19 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una variable que contenga tu altura en centímetros (entero)
+let alturaCentimetros = 177
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaMetros = 1.77
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let kilogramos = 78.3
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRedondeada = Math.ceil(alturaMetros)
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let kilogramosRedondeada = Math.floor(kilogramos)
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let valorMaximo = Number.MAX_VALUE +1 === Number.MAX_VALUE
